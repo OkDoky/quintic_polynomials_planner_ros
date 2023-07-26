9b809bb633854173ead7f5c543aa4e5755212f32
OkDoky <ie5630jk@naver.com>
2023-07-26 21:31:57 +0900

    change T calculation

--- src/quintic_polynomial.cpp
+++ src/quintic_polynomial.cpp
@@ -1,7 +1,7 @@
 #include "quintic_polynomials_planner_ros/quintic_polynomial.h"
 
 QuinticPolynomialsPlanner::QuinticPolynomialsPlanner(double max_speed, double max_throttle) 
-    : max_speed_(max_speed), max_throttle_(max_throttle), speed_epsilon_(0.5), 
+    : max_speed_(max_speed), max_throttle_(max_throttle), speed_epsilon_(0.05), 
     xy_goal_tolerance_(0.05), feedback_epsilon_(0.05){
   std::cout << "[QuinticPolynomialPlanner] init speed : " << max_speed_ << ", throttle : " << max_throttle_ << std::endl;
 }
@@ -9,7 +9,7 @@ QuinticPolynomialsPlanner::QuinticPolynomialsPlanner(double max_speed, double ma
 QuinticPolynomialsPlanner::~QuinticPolynomialsPlanner(){}
 
 QuinticPolynomialsPlanner::QuinticPolynomialsPlanner()
-    : max_speed_(0.0), max_throttle_(0.0), speed_epsilon_(0.5), 
+    : max_speed_(0.0), max_throttle_(0.0), speed_epsilon_(0.05), 
     xy_goal_tolerance_(0.05), feedback_epsilon_(0.05){
   std::cout << "[QuinticPolynomialPlanner] only init object.." << std::endl;
 }
@@ -17,7 +17,7 @@ QuinticPolynomialsPlanner::QuinticPolynomialsPlanner()
 void QuinticPolynomialsPlanner::initialize(double max_speed, double max_throttle){
   max_speed_ = max_speed;
   max_throttle_ = max_throttle;
-  speed_epsilon_ = 0.5;
+  speed_epsilon_ = 0.05;
   xy_goal_tolerance_ = 0.5;
   feedback_epsilon_ = 0.05;
   max_time_ = 15.0;
@@ -33,22 +33,26 @@ void QuinticPolynomialsPlanner::calculateTrackingTime(const geometry_msgs::PoseS
                               global_pose.pose.position.y - goal_pose.pose.position.y);
   double goal_to_end_plan = hypot(goal_pose.pose.position.x - global_plan.back().pose.position.x,
                                   goal_pose.pose.position.y - global_plan.back().pose.position.y);
-  double pp_dist = calPerpendicularDistance(global_plan[0].pose.position.x, global_plan[0].pose.position.y,
-      global_plan.back().pose.position.x, global_plan.back().pose.position.y,
-      global_pose.pose.position.x, global_pose.pose.position.y,
-      goal_pose.pose.position.x, goal_pose.pose.position.y);
+  // double pp_dist = calPerpendicularDistance(global_plan[0].pose.position.x, global_plan[0].pose.position.y,
+  //     global_plan.back().pose.position.x, global_plan.back().pose.position.y,
+  //     global_pose.pose.position.x, global_pose.pose.position.y,
+  //     goal_pose.pose.position.x, goal_pose.pose.position.y);
   double v = feedback_vel.linear.x;
-  if(goal_to_end_plan <= xy_goal_tolerance_){
+  if(goal_to_end_plan <= xy_goal_tolerance_ && abs(v) <=max_speed_-speed_epsilon_){
     // deceleration
-    if (abs(v) < 0.1){
-        T = 2 * sqrt(dist_to_goal);
+    if (abs(v) < 0.1 || v <= 0.0){
+      T = 2 * sqrt(dist_to_goal);
     }else{
-      T = ((dist_to_goal - (1/2)*pow(v, 2)/max_throttle_)/v) + (v/max_throttle_);
+      if (v < speed_epsilon_){
+        v = speed_epsilon_;
+      }
+      T = ((dist_to_goal - (v*v)/(2*max_throttle_))/v) + (v/max_throttle_);
+      std::cout << "[QuinticPolynomialPlanner] [deceldown] T is over : " << T << ", v is : " << v << ", remain dist : " << dist_to_goal << std::endl;
     }
   }else{
     // acceleration
-    if (v <= max_speed_-speed_epsilon_){
-      T = ((max_speed_ - v)/max_throttle_) + ((dist_to_goal - (pow(max_speed_,2)-pow(v,2))/(2*max_throttle_))/max_speed_);
+    if (abs(v) <= max_speed_-speed_epsilon_){
+      T = ((max_speed_ - v)/max_throttle_) + ((dist_to_goal - (max_speed_*max_speed_-v*v)/(2*max_throttle_))/max_speed_);
     } else{
       if (abs(v) < 0.001){
         v = 0.001;
@@ -81,10 +85,6 @@ void QuinticPolynomialsPlanner::getPolynomialPlan(const geometry_msgs::PoseStamp
   tpy = goal_pose.pose.position.y;
 
   double cyaw, tyaw;
-  cyaw = tf2::getYaw(global_pose.pose.orientation);
-  cvx = (feedback_vel.linear.x + feedback_epsilon_) * cos(cyaw);
-  cvy = (feedback_vel.linear.x + feedback_epsilon_) * sin(cyaw);
-
   if (!free_target_vector){
     tyaw = atan2(global_plan.back().pose.position.y -global_plan[0].pose.position.y,
                 global_plan.back().pose.position.x -global_plan[0].pose.position.x);
@@ -94,6 +94,14 @@ void QuinticPolynomialsPlanner::getPolynomialPlan(const geometry_msgs::PoseStamp
   }
   tvx = cos(tyaw);
   tvy = sin(tyaw);
+
+  cyaw = tf2::getYaw(global_pose.pose.orientation);
+  if (abs(abs(cyaw) - abs(tyaw)) > 0.5236){ // 30 degrees
+    cyaw = tyaw;  
+  }
+  cvx = (feedback_vel.linear.x + feedback_epsilon_) * cos(cyaw);
+  cvy = (feedback_vel.linear.x + feedback_epsilon_) * sin(cyaw);
+
   tax = 0.0;
   tay = 0.0;
   cax = 0.0;
@@ -116,13 +124,14 @@ void QuinticPolynomialsPlanner::getPolynomialPlan(const geometry_msgs::PoseStamp
   int num_point = static_cast<int>(T*10); // 10 is hz of local planner..
   double x,y;
   std::vector<geometry_msgs::PoseStamped> polynomial_plan;
-  for (int i = 1; i <= num_point; i++){
+  for (int i = 0; i < num_point; i++){
     x = outputPolynomial(i*0.1, coefficients_x);
     y = outputPolynomial(i*0.1, coefficients_y);
     temp_pose.pose.position.x = x;
     temp_pose.pose.position.y = y;
     polynomial_plan.push_back(temp_pose);
   }
+  polynomial_plan.push_back(goal_pose);
   splineSmooth(polynomial_plan, smooth_plan);
 
 }
